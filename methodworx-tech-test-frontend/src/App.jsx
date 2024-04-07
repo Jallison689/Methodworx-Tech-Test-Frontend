2d2be9019a91d219530aa88f8c3792924f78df0e
Jallison689 <33059158+Jallison689@users.noreply.github.com>
2024-04-07 18:28:33 +0100

    planning

--- methodworx-tech-test-frontend/src/App.jsx
+++ methodworx-tech-test-frontend/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
 
 function App() {
-  const [data, setData] = useState([])
-
-    useEffect(() => {
-        fetchData();
-    }, []);
 
     const fetchData = async () => {
         try {
@@ -24,13 +19,12 @@ function App() {
           </div>
           <h1>Products</h1>
           <div className="card">
-              <table>
-                  <tr>
-                  </tr>
-              </table>
-              <p>
-                  Edit <code>src/App.jsx</code> and save to test HMR
-              </p>
+              <code>
+                  //dynamically generate the table of products with the additions of the add, details and stock additions
+                  //have the basket button up in the corner
+                  //have a model pop up when basket is clicked containing the basket
+                  //change to checkout page when checkout clicked
+              </code>
           </div>
           <p className="read-the-docs">
               Click on the Vite and React logos to learn more
